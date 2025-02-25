d7717ea0f220513072bb977647a7fa5e45c0ff9a
Nick-Petruccelli <nickpetro18@gmail.com>
2025-02-25 12:07:36 -0500

    added get neighbor function

--- inc/graph_crawler.hpp
+++ inc/graph_crawler.hpp
@@ -2,13 +2,16 @@
 #include <string>
 #include <vector>
 
-class GraphCrawler{
+class GraphCrawler {
 public:
-	GraphCrawler(void);
-	std::vector<std::string> bfs(std::string src, std::string dst);
+  GraphCrawler(void);
+  std::vector<std::string> bfs(std::string src, std::string dst);
+
 private:
-	std::vector<std::string> get_neibors(std::string node);
-	const char* get_json(std::string node);
+  std::vector<std::string> get_neibors(std::string node);
+  const char *get_json(std::string node);
+
 private:
-	std::string m_server_addr = "http://hollywood-graph-crawler.bridgesuncc.org/neighbors/";
+  std::string m_server_addr =
+      "http://hollywood-graph-crawler.bridgesuncc.org/neighbors/";
 };
--- src/graph_crawler.cpp
+++ src/graph_crawler.cpp
@@ -1,53 +1,66 @@
 #include "../inc/graph_crawler.hpp"
 #include <cassert>
 #include <cstdlib>
+#include <cstring>
+#include <curl/curl.h>
 #include <iostream>
+#include <rapidjson/document.h>
 #include <string>
 #include <vector>
-#include <curl/curl.h>
-#include <cstring>
 
 typedef struct reponse_chunk {
-	char* mem = (char*)malloc(sizeof(char));
-	size_t size = 1;
-}ResponseChunk;
+  char *mem = (char *)malloc(sizeof(char));
+  size_t size = 1;
+} ResponseChunk;
 
-static size_t write_callback(char *contents, size_t size, size_t nmemb, void *write_chunk){
-	ResponseChunk *chunk = (ResponseChunk*)write_chunk;
-	size_t real_size = size * nmemb;
-	chunk->size = real_size;
-	char * ptr = (char*)realloc(chunk->mem, real_size+1);
-	assert(ptr != NULL);
-	chunk->mem = ptr;
-	memcpy(chunk->mem, contents, real_size);
-	return real_size;
+static size_t write_callback(char *contents, size_t size, size_t nmemb,
+                             void *write_chunk) {
+  ResponseChunk *chunk = (ResponseChunk *)write_chunk;
+  size_t real_size = size * nmemb;
+  chunk->size = real_size;
+  char *ptr = (char *)realloc(chunk->mem, real_size + 1);
+  assert(ptr != NULL);
+  chunk->mem = ptr;
+  memcpy(chunk->mem, contents, real_size);
+  return real_size;
 }
 
-GraphCrawler::GraphCrawler(){
-	curl_global_init(CURL_GLOBAL_ALL);
+GraphCrawler::GraphCrawler() { curl_global_init(CURL_GLOBAL_ALL); }
+
+const char *GraphCrawler::get_json(std::string node) {
+  std::cout << "getting json" << std::endl;
+  CURL *curl = curl_easy_init();
+  assert(curl != NULL);
+  CURLcode res;
+  std::string url = m_server_addr + node;
+  std::cout << url << std::endl;
+  curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+  ResponseChunk res_chunk;
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &res_chunk);
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+  res = curl_easy_perform(curl);
+  std::cout << "result: " << std::endl;
+  std::cout << res_chunk.mem << std::endl;
+  return res_chunk.mem;
 }
 
-const char* GraphCrawler::get_json(std::string node){
-	std::cout << "getting json" << std::endl;
-	CURL* curl = curl_easy_init();
-	assert(curl != NULL);
-	CURLcode res;
-	std::string url = m_server_addr+node;
-	std::cout << url << std::endl;
-	curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-	ResponseChunk res_chunk;
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &res_chunk);
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-	res = curl_easy_perform(curl);
-	std::cout << "result: " << std::endl;
-	std::cout << res_chunk.mem << std::endl;
-	return res_chunk.mem;
+std::vector<std::string> GraphCrawler::get_neibors(std::string node) {
+  const char *json = get_json(node);
+  rapidjson::Document doc;
+  doc.Parse(json);
+  assert(doc.IsObject());
+  assert(doc.HasMember("neighbors"));
+  std::vector<std::string> out;
+  for (auto &neighbor : doc["neighbors"].GetArray()) {
+    out.push_back(neighbor.GetString());
+  }
+  return out;
 }
 
-std::vector<std::string> GraphCrawler::bfs(std::string src, std::string dst){
-	std::vector<std::string> out;
-
-	get_json(src);
-
-	return out;	
+std::vector<std::string> GraphCrawler::bfs(std::string src, std::string dst) {
+  std::vector<std::string> neighbors = get_neibors(src);
+  for (std::string neighbor : neighbors) {
+    std::cout << neighbor << std::endl;
+  }
+  return neighbors;
 };
