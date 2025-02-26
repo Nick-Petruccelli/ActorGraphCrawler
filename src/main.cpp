5bbe559a342a7f67d351b2c175c564d2470f4f32
NickPetruccelli <NickPetro18@gmail.com>
2025-02-26 15:58:13 -0500

    added timing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "../inc/graph_crawler.hpp"
+#include <chrono>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -22,11 +23,17 @@ int main(int argc, char * argv[]) {
   }
 
   GraphCrawler gc;
+  std::chrono::high_resolution_clock clock;
+  std::chrono::time_point<std::chrono::high_resolution_clock> start = clock.now();
   std::vector<std::string> path = gc.bfs(src, dst, depth);
+  std::chrono::time_point<std::chrono::high_resolution_clock> end = clock.now();
 
-  for (std::string node : path) {
-    std::cout << node << std::endl;
-  }
+  std::chrono::duration ns = end - start;
+  double time = ns.count() / 1E9;
+
+
+  std::cout << "Src: " << src << " Dst: " << dst << " Depth: " << depth << std::endl;
+  std::cout << "Execution Time: " << time << std::endl;
 
   return 0;
 };
