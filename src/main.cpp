9e274d56f030bd3b60e70a6fc55a682bf5fbaf5c
Nick-Petruccelli <nickpetro18@gmail.com>
2025-02-26 12:29:06 -0500

    added path retracing

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include "../inc/graph_crawler.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
 
 int main() {
   GraphCrawler gc;
-  gc.bfs("Tom Hanks", "Tom Hanks", 10);
+  std::vector<std::string> path = gc.bfs("Tom Hanks", "The Green Mile", 10);
+  for (std::string node : path) {
+    std::cout << node << std::endl;
+  }
 
   return 0;
 };
